58246c96370c531be17826273b6f720a0c0d47ed
gautam kumar <krgautam7777@gmail.com>
2024-01-03 13:20:51 +0530

    first commit

---
+++ server/models/User.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cart: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+      price: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
